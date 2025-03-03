4e9e34fb7f9ae4a2c6f26ae902412c60d6e7775d
dhernandez-2020416 <dhernandez-2020416@kinal.edu.gt>
2025-03-02 22:14:33 -0600

    Se añaden las rutas de shopping cart e invoice al proyecto

--- configs/app.js
+++ configs/app.js
@@ -9,6 +9,8 @@ import authRoutes from '../src/auth/auth.routes.js'
 import userRoutes from '../src/user/user.routes.js'
 import categoryRoutes from '../src/categories/category.routes.js'
 import productRoutes from '../src/product/product.routes.js'
+import shoppingCartRoutes from '../src/shoppingCart/shoppingCart.routes.js'
+import invoiceRoutes from '../src/invoice/invoice.routes.js'
 
 const configs = (app) =>{
     app.use(express.json())
@@ -24,6 +26,8 @@ const routes = (app) =>{
     app.use('/v1/user', userRoutes)
     app.use('/v1/category', categoryRoutes)
     app.use('/v1/product', productRoutes)
+    app.use('/v1/shoppingCart', shoppingCartRoutes)
+    app.use('/v1/invoice', invoiceRoutes)
 }
 
 export const initServer = async() =>{
